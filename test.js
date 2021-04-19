dab03873e8c39e16c9739208801ce7cc45c92a89
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2021-04-19 22:24:27 +0200

    feat: replace tinycolor2 with colord (#36)

--- test.js
+++ test.js
@@ -25,8 +25,8 @@ test('the convert fn converts colors to an HSLA object', t => {
 	colors.forEach(color => {
 		t.deepEqual(color, {
 			hue: 0,
-			saturation: 1,
-			lightness: 0.5,
+			saturation: 100,
+			lightness: 50,
 			alpha: 1,
 			authored: color.authored
 		})
@@ -57,14 +57,14 @@ test('Colors are sorted by Hue, then by saturation', t => {
 	const colors = [
 		'hsl(0, 100%, 50%)',
 		'hsl(0, 50%, 50%)',
-		'hsl(50, 50%, 50%)',
+		'hsl(50, 20%, 50%)',
 		'hsl(50, 100%, 50%)'
 	]
 	const expected = [
 		'hsl(0, 50%, 50%)',
 		'hsl(0, 100%, 50%)',
-		'hsl(50, 100%, 50%)',
-		'hsl(50, 50%, 50%)'
+		'hsl(50, 20%, 50%)',
+		'hsl(50, 100%, 50%)'
 	]
 	const actual = colorSorter(colors)
 
@@ -133,6 +133,62 @@ test('Fully transparent colors are shifted to the end', t => {
 	const colors = ['hsla(0, 0, 0, 0)', 'hsla(0, 0, 0, .5)', 'hsla(0, 0, 0, 1)']
 	const actual = colorSorter(colors)
 	const expected = ['hsla(0, 0, 0, 1)', 'hsla(0, 0, 0, .5)', 'hsla(0, 0, 0, 0)']
+
+	t.deepEqual(actual, expected)
+})
+
+test('smoke test', t => {
+	const colors = [
+		'#4b4747',
+		'#d70c0b',
+		'#f00',
+		'#f22b24',
+		'#ff6930',
+		'#eb6c1e',
+		'#eb6d1e',
+		'#f57917',
+		'#ff8a0a',
+		'#f7a336',
+		'#feb95a',
+		'#eca920',
+		'#f1c15d',
+		'#f1c260',
+		'#ff0',
+		'#c8d05b',
+		'#ccd557',
+		'#d2ff52',
+		'#10ac47',
+		'#04a03b',
+		'#03fff3',
+		'#25bbc3',
+		'#38d7df',
+		'#15b8ec',
+		'#00adea',
+		'#8e34c9',
+		'#9a3dd1',
+		'#cd66f6',
+		'#fff',
+		'rgba(255,255,255,0.2)',
+		'rgba(255,255,255,0.07)',
+		'#f9f9f9',
+		'#f4f4f4',
+		'#f2f2f2',
+		'#e4e4e4',
+		'#ddd',
+		'#c0c0c0',
+		'#666',
+		'#4a4a4a',
+		'#1d1d1d',
+		'#0d0d0d',
+		'#000',
+		'rgba(0,0,0,0.8)',
+		'rgba(0,0,0,0.6)',
+		'rgba(0,0,0,0.4)',
+		'rgba(0,0,0,0.1)',
+		'rgba(0,0,0,0.05)'
+	]
+	const expected = [...colors]
+	const actual = colorSorter(colors)
 
 	t.deepEqual(actual, expected)
 })
