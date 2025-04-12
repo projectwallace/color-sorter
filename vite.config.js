17fb08debc9a89b01b22acf530795611ef80dbbe
Bart Veneman <bart.veneman@drukwerkdeal.nl>
2025-04-12 20:12:34 +0200

    BREAKING: ship ESM only

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: resolve(__dirname, "index.js"),
-			name: "colorSorter",
-			fileName: "color-sorter",
+			formats: ['es']
 		},
 		rollupOptions: {
 			// make sure to externalize deps that shouldn't be bundled
