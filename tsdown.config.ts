796de09c6f52e89de8c3a8d410e3f8a6e267ad92
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2026-03-09 20:29:16 +0100

    Change bundleName from 'formatCss' to 'colorSorter' (#111)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	plugins: [
 		codecovRollupPlugin({
 			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-			bundleName: "formatCss",
+			bundleName: "colorSorter",
 			uploadToken: process.env.CODECOV_TOKEN,
 		}),
 	],
