fb7529738b0ff377444a55924e4e63d53d0d0a3c
Bart Veneman <1536852+bartveneman@users.noreply.github.com>
2026-08-01 22:08:52 +0200

    Align GitHub Actions with other projectwallace repositories (#148)
    - remove codecov integration
    - dd e18e dependency diff integration
    - lock down permissions
    - use multi-step release process to avoid leaking secrets
    - add 7 day cooldown to dependabot

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'tsdown'
-import { codecovRollupPlugin } from '@codecov/rollup-plugin'
 
 export default defineConfig({
 	entry: ['index.ts'],
@@ -7,11 +6,4 @@ export default defineConfig({
 	platform: 'neutral',
 	dts: true,
 	publint: true,
-	plugins: [
-		codecovRollupPlugin({
-			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-			bundleName: 'colorSorter',
-			uploadToken: process.env.CODECOV_TOKEN,
-		}),
-	],
 })
